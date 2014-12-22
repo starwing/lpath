9f3e706954c5d61cc8a888a4613bd3508fa62ca1
StarWing <weasley.wx@gmail.com>
2014-12-22 14:31:21 +0800

    change test suite

--- test.lua
+++ test.lua
@@ -1,4 +1,15 @@
-local P = require 'path'
+local P = require "path"
+local fs = require "path.fs"
+local info = require "path.info"
+
+local keys = {}
+for k,v in pairs(info) do
+   keys[#keys+1] = k
+end
+table.sort(keys)
+for _, k in ipairs(keys) do
+   print(k, info[k])
+end
 
 local test_dir = {
     name = "test";
@@ -24,47 +35,53 @@ local tests = {}
 function add_test(name, f)
     tests[#tests+1] = function()
         print("[TEST] "..name)
-        assert(P[name], "required "..name.." in lpath")
         f()
         print("[ OK ] "..name)
     end
 end
 
-add_test("mkdir",     function ()
-    P.rmdir_rec "test"
-    assert(P.mkdir "test")
-    assert(P.exists "test")
-    assert(P.isdir "test")
+-- path
+
+add_test("abs", function ()
+end)
+add_test("itercomp", function ()
+end)
+add_test("join", function ()
+end)
+add_test("normcase", function ()
+end)
+add_test("normpath", function ()
+end)
+add_test("realpath", function ()
+end)
+add_test("rel", function ()
+end)
+add_test("split", function ()
+end)
+add_test("splitdrive", function ()
+end)
+add_test("splitext", function ()
+end)
+add_test("type", function ()
 end)
 
-add_test("mkdir_rec", function ()
-    local function _dfs(d)
-        assert(P.mkdir(assert(d.name)))
-        assert(P.chdir(assert(d.name)))
-        for k, v in ipairs(d) do
-            if type(v) == 'string' then
-                assert(P.touch(v))
-            else
-                _dfs(v)
-            end
-        end
-        assert(P.chdir "..")
-    end
-    local cwd = assert(P.getcwd())
-    assert(P.chdir "test/..")
-    _dfs(test_dir)
-    assert(P.chdir(cwd))
-end)
+-- path.fs
 
-add_test("dir",       function ()
-    local cwd = assert(P.getcwd())
-    assert(P.chdir "test/..")
+add_test("chdir", function ()
+end)
+add_test("cmpftime", function ()
+end)
+add_test("copy", function ()
+end)
+add_test("dir", function ()
+    local cwd = assert(fs.getcwd())
+    assert(fs.chdir "test/..")
     local function _check(d)
-        assert(P.chdir(assert(d.name)))
+        assert(fs.chdir(assert(d.name)))
         local idx = 1
-        for fn, ft in P.dir '.' do
+        for fn, ft in fs.dir '.' do
             if fn == '.' or fn == '..' then goto next end
-            if ft == 'dir' or (ft == nil and P.isdir(fn)) then
+            if ft == 'dir' or (ft == nil and fs.type(fn) == "dir") then
                 assert(d[idx].name == fn, "idx = "..idx..", name = "..tostring(d[idx].name)..", fn = "..fn)
                 _check(d[idx])
             else
@@ -73,48 +90,73 @@ add_test("dir",       function ()
             idx = idx + 1
             ::next::
         end
-        assert(P.chdir "..")
+        assert(fs.chdir "..")
     end
     _check(test_dir)
-    assert(P.chdir(cwd))
+    assert(fs.chdir(cwd))
 end)
-
-add_test("isdir",     function ()
+add_test("exists", function ()
 end)
-add_test("chdir",     function ()
+add_test("fsize", function ()
 end)
-add_test("rmdir",     function ()
+add_test("ftime", function ()
 end)
-add_test("rmdir_rec", function ()
+add_test("getcwd", function ()
 end)
-add_test("exists",    function ()
+add_test("mkdir", function ()
+    fs.removedirs "test"
+    assert(fs.mkdir "test")
+    assert(fs.exists "test")
+    assert(fs.type "test" == "dir")
 end)
-add_test("getcwd",    function ()
+add_test("makedirs", function ()
+    local function _dfs(d)
+        assert(fs.mkdir(assert(d.name)))
+        assert(fs.chdir(assert(d.name)))
+        for k, v in ipairs(d) do
+            if type(v) == 'string' then
+                assert(fs.touch(v))
+            else
+                _dfs(v)
+            end
+        end
+        assert(fs.chdir "..")
+    end
+    local cwd = assert(fs.getcwd())
+    assert(fs.chdir "test/..")
+    _dfs(test_dir)
+    assert(fs.chdir(cwd))
 end)
-add_test("filetime",  function ()
+add_test("platform", function ()
+    local function _dfs(d)
+        assert(fs.mkdir(assert(d.name)))
+        assert(fs.chdir(assert(d.name)))
+        for k, v in ipairs(d) do
+            if type(v) == 'string' then
+                assert(fs.touch(v))
+            else
+                _dfs(v)
+            end
+        end
+        assert(fs.chdir "..")
+    end
+    local cwd = assert(fs.getcwd())
+    assert(fs.chdir "test/..")
+    _dfs(test_dir)
+    assert(fs.chdir(cwd))
 end)
-add_test("filesize",  function ()
+add_test("remove", function ()
 end)
-add_test("cmptime",   function ()
+add_test("removedirs", function ()
 end)
-add_test("touch",     function ()
+add_test("rmdir", function ()
 end)
-add_test("abspath",   function ()
+add_test("setenv", function ()
 end)
-add_test("relpath",   function ()
+add_test("touch", function ()
 end)
-add_test("normpath",  function ()
-end)
-add_test("joinpath",  function ()
-end)
-add_test("splitpath", function ()
-end)
-add_test("splitext",  function ()
-end)
-add_test("iterpath",  function ()
-end)
-add_test("walkpath",  function ()
+add_test("walk", function ()
 end)
 
 for _,v in ipairs(tests) do v() end
-assert(P.rmdir_rec "test")
+assert(fs.removedirs "test")
